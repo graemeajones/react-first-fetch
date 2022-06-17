692a451b1be8162b700001a2da3e6e1e5e2b3bad
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2022-06-17 22:28:41 +0100

    Read for release (#1)
    Co-authored-by: GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>

--- src/components/api/apiRequest.js
+++ src/components/api/apiRequest.js
@@ -1,21 +1,30 @@
-// -----------------------------------------------------------------------
-// apiURL: e.g. "https://my.api.mockaroo.com"
-// key: e.g. "?key=bb6adbc0" (Only seen this used for the mockaroo site.)
-// endpoint: e.g. "Modules" or "/User/123"
-// method: defaults to "GET" if not specified. (Use this later when trying to add or, modify or delete records.)
-// body: defaults to null if not specified. (Use this later when you want to update records.)
-// -----------------------------------------------------------------------
+/*
+ * --------------------------------------------------------------------------------------------------------
+ * Function that makes and sends network requests to the server i.e. API
+ * @param  {String}  apiURL    Specifiies URL of API domain e.g. "https://my.api.mockaroo.com"
+ * @param  {String}  endpoint  Specifies service at the API e.g. "Modules" or "/Users/3"
+ * @param  {String}  key       Optional string for any key  e.g. "?key=bb6adbc0" for Graeme's mockaroo site
+ * @param  {String}  method    Choose GET, POST, PUT, DELETE - default is GET
+ * @param  {String}  body      Object containing data to be sent to API endpoint e.g. a new module
+ * @return {Object}            { success: true or false, response: a JSON data object from the API }
+ * --------------------------------------------------------------------------------------------------------
+ */
 
 export async function apiRequest(apiURL, endpoint, key, method = "GET", body = null) {
 
   // Build fetch parameters
   let requestObj = { method: method }; // *GET, POST, PUT, DELETE, etc.
   if (body) requestObj = { ...requestObj, body: body };
-
-  // Call API and return response object
   const endpointAddress = apiURL + endpoint + key;
-  const response = await fetch(endpointAddress, requestObj);
-  if ((response.status >= 200) && (response.status <= 299)) 
-       return { success: true, response: await response.json() };
-  else return { success: false, response: response };
+
+  // Run fetch and build and return response object
+  try {
+    const response = await fetch(endpointAddress, requestObj);
+    return (response.status >= 200) && (response.status <= 299)
+      ? { success: true, response: await response.json() }
+      : { success: false, response: response }
+
+  } catch (error) {
+    throw new Error(`Error in apiRequest: ${error}`);
+  }
 }
